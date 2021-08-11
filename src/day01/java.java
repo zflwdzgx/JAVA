1baa2997aae556809edcd398f758e638734c2881
zfl <1016067729@qq.com>
2021-08-12 00:39:34 +0800

    ZFL

---
+++ src/day01/java.java
@@ -0,0 +1,14 @@
+package day01;
+
+public class java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("helloword");
+		int count=1;
+		for(int i=0;i<6;i++){
+			System.out.println(count++);
+		};
+	}
+
+}
---
+++ src/day02/java01.java
@@ -0,0 +1,13 @@
+package day02;
+/*
+ * @author
+ */
+
+public class java01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("laozhang");
+	}
+
+}
